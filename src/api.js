e081bf35829d0fc6824bf181c0944b9576a8443d
savickuse <savickuse@users.noreply.github.com>
2025-10-05 18:45:48 +0300

    perf: refactor module structure

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 58
 module.exports = API;
+
+
+// API module for PancakeStack
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 60
+module.exports = API;
