b88dc027f426eccab953255edbc709615b560b44
savickuse <savickuse@users.noreply.github.com>
2025-10-12 18:29:35 +0300

    test: fix async handling

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 60
 module.exports = API;
+
+
+// API module for PancakeStack
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 75
+module.exports = API;
