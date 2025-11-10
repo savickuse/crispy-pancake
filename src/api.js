2004e3d6a5d4502461a09436b460f22301a2e298
savickuse <savickuse@users.noreply.github.com>
2025-11-11 00:18:41 +0200

    update(ui): add error messages

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 57
 module.exports = API;
+
+
+// API module for PancakeStack
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 58
+module.exports = API;
