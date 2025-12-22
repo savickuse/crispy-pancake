922acc0c4e6bcc74626489790e30da07a04ef022
savickuse <savickuse@users.noreply.github.com>
2025-12-22 07:45:48 +0200

    style(core): CI/CD pipeline changes

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 44
 module.exports = API;
+
+
+// API module for PancakeStack
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 57
+module.exports = API;
