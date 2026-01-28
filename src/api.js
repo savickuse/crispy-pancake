f83335aa67ab6926e76f0ba62b1bbae58e58589d
savickuse <savickuse@users.noreply.github.com>
2026-01-28 11:24:30 +0200

    refactor: improve user feedback

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 75
 module.exports = API;
+
+
+// API module for PancakeStack
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 77
+module.exports = API;
